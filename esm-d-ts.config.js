13b05541c811949cbfaa9aae9ddf36071ddac660
typhonrt <support@typhonjs.io>
2023-06-12 17:46:27 -0700

    comments

--- esm-d-ts.config.js
+++ esm-d-ts.config.js
@@ -1,5 +1,12 @@
 // Generate DTS for generator / transformer exports.
-export default [
+
+// Note: to reference GenerateConfig when installing `esm-d-ts` use:
+// `import('@typhonjs-build-test/esm-d-ts').GenerateConfig)`
+
+/** @type {import('./src/generator').GenerateConfig[]} */
+const configs = [
    { input: './src/generator/index.js', output: './src/generator/index.d.ts' },
    { input: './src/transformer/index.js', output: './src/transformer/index.d.ts' }
 ];
+
+export default configs;
