025e14fb2ae920ef6b577e8bef608cfffa643109
typhonrt <support@typhonjs.io>
2023-06-15 03:17:41 -0700

    use automatic output configuration.

--- esm-d-ts.config.js
+++ esm-d-ts.config.js
@@ -1,12 +1,19 @@
-// Generate DTS for generator / transformer exports.
-
-// Note: to reference GenerateConfig when installing `esm-d-ts` use:
-// `import('@typhonjs-build-test/esm-d-ts').GenerateConfig)`
+/**
+ * Generate DTS for generator / transformer exports.
+ *
+ * When not explicitly settings `output` `esm-d-ts` will automatically configure `output` by changing the extension
+ * of the required input path. With the config below output for the first and second bundles are:
+ * - `./src/generator/index.d.ts`
+ * - `./src/transformer/index.d.ts`
+ *
+ * Note: to reference GenerateConfig when installing `esm-d-ts` use:
+ * `import('@typhonjs-build-test/esm-d-ts').GenerateConfig)`
+ */
 
 /** @type {import('./src/generator').GenerateConfig[]} */
 const configs = [
-   { input: './src/generator/index.js', output: './src/generator/index.d.ts' },
-   { input: './src/transformer/index.js', output: './src/transformer/index.d.ts' }
+   { input: './src/generator/index.js' },
+   { input: './src/transformer/index.js' }
 ];
 
 export default configs;
