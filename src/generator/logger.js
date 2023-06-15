5dc5dc39db4edf1a0d82be78efb48871ce139460
typhonrt <support@typhonjs.io>
2023-06-14 18:47:28 -0700

    make `logLevels` static public

--- src/generator/logger.js
+++ src/generator/logger.js
@@ -8,7 +8,7 @@ export class Logger
     *
     * @type {{ error: number, warn: number, all: number }}
     */
-   static #logLevels = {
+   static logLevels = {
       error: 2,
       warn: 1,
       all: 0
@@ -33,7 +33,7 @@ export class Logger
     */
    static info(message, configLevel)
    {
-      if (this.#logLevels[configLevel] > this.#logLevels.all) { return; }
+      if (this.logLevels[configLevel] > this.logLevels.all) { return; }
 
       console.log(`[esm-d-ts] ${message}`);
    }
@@ -47,7 +47,7 @@ export class Logger
     */
    static warn(message, configLevel)
    {
-      if (this.#logLevels[configLevel] > this.#logLevels.warn) { return; }
+      if (this.logLevels[configLevel] > this.logLevels.warn) { return; }
 
       console.warn(`[33m[esm-d-ts] ${message}[0m`);
    }
