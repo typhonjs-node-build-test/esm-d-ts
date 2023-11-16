358011d0491d0b74fbeb05c1b0ca930de1a58ff4
typhonrt <support@typhonjs.io>
2023-11-16 05:11:51 -0800

    remove deleted files

--- src/post-process/index.js
+++
@@ -1,8 +0,0 @@
-export * from './GraphAnalysis.js';
-export * from './PostProcess.js';
-
-/**
- * @typedef {(
- *    (params: { sourceFile?: import('ts-morph').SourceFile, inheritance?: import('./GraphAnalysis').GraphAnalysis }) => void
- * )} ProcessorFunction
- */
