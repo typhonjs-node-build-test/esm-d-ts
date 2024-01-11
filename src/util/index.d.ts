e95c0d783317d16c8d79533f61c5c7b5694838e1
typhonrt <support@typhonjs.io>
2024-01-11 09:19:21 -0800

    comments.

--- src/util/index.d.ts
+++ src/util/index.d.ts
@@ -1,7 +1,10 @@
 import * as _typhonjs_utils_logger_color from '@typhonjs-utils/logger-color';
 
 /**
+ * Provides a ColorLogger instance accessible across the package.
+ *
  * @type {import('@typhonjs-utils/logger-color').ColorLogger}
+ * @see https://typhonjs-node-utils.github.io/logger-color/classes/_typhonjs_utils_logger_color.ColorLogger.html
  */
 declare const logger: _typhonjs_utils_logger_color.ColorLogger;
 
--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,10 @@
 import { ColorLogger } from '@typhonjs-utils/logger-color';
 
 /**
+ * Provides a ColorLogger instance accessible across the package.
+ *
  * @type {import('@typhonjs-utils/logger-color').ColorLogger}
+ * @see https://typhonjs-node-utils.github.io/logger-color/classes/_typhonjs_utils_logger_color.ColorLogger.html
  */
 const logger = new ColorLogger({ tag: 'esm-d-ts' });
 
