42e0f05fc80cf211639b75655573e61e30fb59c8
typhonrt <support@typhonjs.io>
2024-01-31 09:34:15 -0800

    Update types

--- src/util/index.d.ts
+++ src/util/index.d.ts
@@ -1,4 +1,5 @@
 import * as _typhonjs_utils_logger_color from '@typhonjs-utils/logger-color';
+import * as comment_parser from 'comment-parser';
 
 /**
  * Provides a ColorLogger instance accessible across the package.
@@ -8,27 +9,37 @@ import * as _typhonjs_utils_logger_color from '@typhonjs-utils/logger-color';
 declare const logger: _typhonjs_utils_logger_color.ColorLogger;
 
 /**
- * Parses a `type` string for `import types` returning an object with the parts required to perform AST manipulation.
- *
- * The resulting object contains:
- * - `identFull` - Fully qualified symbol.
- * - `identImport` - The first part of the imported symbol.
- * - `module` - The module / package imported.
+ * Parses a string for `import types` returning an object with the parts required to perform AST manipulation.
  *
  * If no import type statement detected the result is undefined.
  *
  * @param {*}  type - A type string to parse for import types.
  *
- * @returns {{ identFull: string, identImport: string, module: string } | undefined} Result of parsed import type or
- *          undefined.
+ * @returns {ParsedImportType | undefined} Result of parsed import type or undefined.
  */
-declare function parseImportType(type: any):
-  | {
-      identFull: string;
-      identImport: string;
-      module: string;
-    }
-  | undefined;
+declare function parseImportType(type: any): ParsedImportType | undefined;
+/**
+ * Parses all import type statements from a parsed comment block given a JSDoc tag to parse for the type.
+ *
+ * @param {object} options - Options.
+ *
+ * @param {import('comment-parser').Block}   options.block - A parsed comment block.
+ *
+ * @param {string} options.tag - The JSDoc tag to parse for import types.
+ *
+ * @param {boolean} [options.first=false] - The JSDoc tag to parse for import types.
+ *
+ * @returns {ParsedImportType[] | undefined} Resulting parsed import types for the given tag.
+ */
+declare function parseImportTypesFromBlock({
+  block,
+  tag,
+  first,
+}: {
+  block: comment_parser.Block;
+  tag: string;
+  first?: boolean;
+}): ParsedImportType[] | undefined;
 /**
  * Matches named import types; IE `import('./types').Value`. It is lenient and allows the import to be enclosed
  * in `()` which helps for `checkJs` support / prevents TS from choking as import types in `@implements` tags is not
@@ -41,5 +52,22 @@ declare function parseImportType(type: any):
  * @type {RegExp}
  */
 declare const regexImportType: RegExp;
+/**
+ * Defines the parts of an import type statement.
+ */
+type ParsedImportType = {
+  /**
+   * Fully qualified symbol including namespace.
+   */
+  identFull: string;
+  /**
+   * The first part of the imported symbol to synthesize a module import.
+   */
+  identImport: string;
+  /**
+   * The module / package imported.
+   */
+  module: string;
+};
 
-export { logger, parseImportType, regexImportType };
+export { type ParsedImportType, logger, parseImportType, parseImportTypesFromBlock, regexImportType };
