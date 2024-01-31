56e3d454e5a3a329637e85e4d3090e23e0b0540a
typhonrt <support@typhonjs.io>
2024-01-31 09:34:02 -0800

    Added `parseImportTypesFromBlock`

--- src/util/parseImportType.js
+++ src/util/parseImportType.js
@@ -1,4 +1,8 @@
-import upath from 'upath';
+import {
+   isIterable,
+   isObject }  from "@typhonjs-utils/object";
+
+import upath   from 'upath';
 
 /**
  * Matches named import types; IE `import('./types').Value`. It is lenient and allows the import to be enclosed
@@ -14,19 +18,13 @@ import upath from 'upath';
 export const regexImportType = /import\(['"]([^'"]+)['"]\)\.([^.\s]+?)(?:\.([^)\s]+?))?(?=\)?$)/;
 
 /**
- * Parses a `type` string for `import types` returning an object with the parts required to perform AST manipulation.
- *
- * The resulting object contains:
- * - `identFull` - Fully qualified symbol.
- * - `identImport` - The first part of the imported symbol.
- * - `module` - The module / package imported.
+ * Parses a string for `import types` returning an object with the parts required to perform AST manipulation.
  *
  * If no import type statement detected the result is undefined.
  *
  * @param {*}  type - A type string to parse for import types.
  *
- * @returns {{ identFull: string, identImport: string, module: string } | undefined} Result of parsed import type or
- *          undefined.
+ * @returns {ParsedImportType | undefined} Result of parsed import type or undefined.
  */
 export function parseImportType(type)
 {
@@ -55,3 +53,57 @@ export function parseImportType(type)
       };
    }
 }
+
+/**
+ * Parses all import type statements from a parsed comment block given a JSDoc tag to parse for the type.
+ *
+ * @param {object} options - Options.
+ *
+ * @param {import('comment-parser').Block}   options.block - A parsed comment block.
+ *
+ * @param {string} options.tag - The JSDoc tag to parse for import types.
+ *
+ * @param {boolean} [options.first=false] - The JSDoc tag to parse for import types.
+ *
+ * @returns {ParsedImportType[] | undefined} Resulting parsed import types for the given tag.
+ */
+export function parseImportTypesFromBlock({ block, tag, first = false })
+{
+   // The comment block is not valid or there are no tags.
+   if (!isObject(block) || !isIterable(block.tags)) { return; }
+
+   if (typeof tag !== 'string') { throw new TypeError(`parseImportTypesFromBlock error: 'tag' is not a string.`); }
+   if (typeof first !== 'boolean')
+   {
+      throw new TypeError(`parseImportTypesFromBlock error: 'first' is not a boolean.`);
+   }
+
+   const results = [];
+
+   for (const entry of block.tags)
+   {
+      if (entry.tag === tag && typeof entry.type === 'string')
+      {
+         const parsedImport = parseImportType(entry.type);
+         if (parsedImport)
+         {
+            results.push(parsedImport);
+
+            // Handle the case when only the first parsed result is requested.
+            if (first) { return results; }
+         }
+      }
+   }
+
+   return results;
+}
+
+/**
+ * @typedef {object} ParsedImportType Defines the parts of an import type statement.
+ *
+ * @property {string} identFull Fully qualified symbol including namespace.
+ *
+ * @property {string} identImport The first part of the imported symbol to synthesize a module import.
+ *
+ * @property {string} module The module / package imported.
+ */
