c204b1005815a9d627fb1a0db36beb9652471989
typhonrt <support@typhonjs.io>
2024-02-10 23:27:44 -0800

    Add config.

---
+++ vitest.config.js
@@ -0,0 +1,18 @@
+import {
+   configDefaults,
+   defineConfig } from 'vitest/config';
+
+export default defineConfig({
+   test: {
+      exclude: [...configDefaults.exclude],
+      include: ['./test/**/*.test.js'],
+      coverage: {
+         include: ['src/**'],
+         exclude: ['test/**'],
+         provider: 'v8',
+         reporter: ['text', 'json', 'html']
+      },
+      reporters: ['default', 'html'],
+      globals: true
+   }
+});
