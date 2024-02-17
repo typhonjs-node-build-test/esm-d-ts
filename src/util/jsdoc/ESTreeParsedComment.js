4b43d7cf3e8b6ff61d5e02f700050905e8e8e9bd
typhonrt <support@typhonjs.io>
2024-02-16 20:30:45 -0800

    Accept iterable

--- src/util/jsdoc/ESTreeParsedComment.js
+++ src/util/jsdoc/ESTreeParsedComment.js
@@ -1,7 +1,9 @@
 import {
    commentParserToESTree,
    estreeToString,
-   parseComment } from '@es-joy/jsdoccomment';
+   parseComment }       from '@es-joy/jsdoccomment';
+
+import { isIterable }   from '@typhonjs-utils/object';
 
 /**
  * Provides a more flexible mechanism to modify JSDoc comment blocks. `comment-parser` is the main parsing mechanism
@@ -32,19 +34,20 @@ export class ESTreeParsedComment
    /**
     * Removes the JSDoc tags specified from the AST.
     *
-    * @param {string | Set<string> | undefined} [tagNames] - A string or Set of strings defining tag names to remove
-    *        from the AST. When undefined _all_ tags are removed.
+    * @param {string | Iterable<string> | undefined} [tagNames] - A string or Set of strings defining tag names to
+    *        remove from the AST. When undefined _all_ tags are removed.
     *
     * @returns {this} This instance.
     */
    removeTags(tagNames)
    {
       const isString = typeof tagNames === 'string';
-      const isSet = tagNames instanceof Set;
+      const isList = isIterable(tagNames);
 
-      if (tagNames !== void 0 && !isString && !isSet)
+      if (tagNames !== void 0 && !isString && !isList)
       {
-         throw new TypeError(`ESTreeParsedComment.removeTags error: 'tagNames' is not a string or Set.`);
+         throw new TypeError(
+          `ESTreeParsedComment.removeTags error: 'tagNames' is not a string or iterable list of strings.`);
       }
 
       const tags = this.#ast.tags;
@@ -56,11 +59,12 @@ export class ESTreeParsedComment
             if (tags[i].tag === tagNames) { tags.splice(i, 1); }
          }
       }
-      else if (isSet)
+      else if (isList)
       {
+         const tagSet = tagNames instanceof Set ? tagNames : new Set(tagNames);
          for (let i = tags.length; --i >= 0;)
          {
-            if (tagNames.has(tags[i].tag)) { tags.splice(i, 1); }
+            if (tagSet.has(tags[i].tag)) { tags.splice(i, 1); }
          }
       }
       else
@@ -74,7 +78,7 @@ export class ESTreeParsedComment
    /**
     * Provides an iterator over all tags or a subset from the given tag names.
     *
-    * @param {string | Set<string>} [tagNames] - A string or Set of strings defining tag names to iterate.
+    * @param {string | Iterable<string>} [tagNames] - A string or Set of strings defining tag names to iterate.
     *
     * @returns {IterableIterator<import('@es-joy/jsdoccomment').JsdocTag>} Tag iterator.
     * @yields {import('@es-joy/jsdoccomment').JsdocTag}
@@ -82,11 +86,11 @@ export class ESTreeParsedComment
    *tags(tagNames)
    {
       const isString = typeof tagNames === 'string';
-      const isSet = tagNames instanceof Set;
+      const isList = isIterable(tagNames);
 
-      if (tagNames !== void 0 && !isString && !isSet)
+      if (tagNames !== void 0 && !isString && !isList)
       {
-         throw new TypeError(`ESTreeParsedComment.tags error: 'tagNames' is not a string or Set.`);
+         throw new TypeError(`ESTreeParsedComment.tags error: 'tagNames' is not a string or iterable list of strings.`);
       }
 
       const tags = this.#ast.tags;
@@ -98,11 +102,12 @@ export class ESTreeParsedComment
             if (tags[i].tag === tagNames) { yield tags[i]; }
          }
       }
-      else if (isSet)
+      else if (isList)
       {
+         const tagSet = tagNames instanceof Set ? tagNames : new Set(tagNames);
          for (let i = 0; i < tags.length; i++)
          {
-            if (tagNames.has(tags[i].tag)) { yield tags[i]; }
+            if (tagSet.has(tags[i].tag)) { yield tags[i]; }
          }
       }
       else
