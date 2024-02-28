a2ad43f6166bb54bf7659df7ef3e14042d957892
typhonrt <support@typhonjs.io>
2024-02-28 00:54:49 -0800

    Initial commit.

---
+++ test/fixture/snapshot/generate/javascript/valid-prettier/index.d.ts
@@ -0,0 +1,56 @@
+/**
+ * A test interface.
+ */
+declare interface ITest {
+	/** A test function */
+	foo(): boolean;
+}
+/**
+ * Namespace for testing jsdocImplementsImportType transformer  w/ `@inherits`.
+ */
+declare namespace NameSpace {
+	/** Templated interface */
+	interface ITest2<T> {
+		bar(): T;
+	}
+}
+/**
+ * A test type alias.
+ */
+type TypeAlias = {
+	/** A test property */
+	bar?: boolean;
+};
+
+/**
+ * A test class that implements ITest
+ *
+ * @implements {(import('./types').ITest)}
+ * @implements {(import('./types').NameSpace.ITest2<boolean>)}
+ */
+declare class Test implements ITest, NameSpace.ITest2<boolean> {
+	/**
+	 * Implementation for ITest.
+	 *
+	 * @returns {boolean}
+	 */
+	foo(): boolean;
+	/**
+	 * Implementation from ITest2.
+	 *
+	 * @returns {boolean}
+	 */
+	bar(): boolean;
+	/**
+	 * Tests setter accessor transformer to ensure argument matches the `@param` name.
+	 *
+	 * @param {boolean}  baz -
+	 */
+	set baz(baz: boolean);
+	/**
+	 * @returns {boolean}
+	 */
+	get baz(): boolean;
+}
+
+export { type ITest, NameSpace, Test, type TypeAlias };
