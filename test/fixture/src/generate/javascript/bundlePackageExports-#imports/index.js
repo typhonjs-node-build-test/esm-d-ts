2b4d52ddd8473f77f5e77698e78ff169656740f6
typhonrt <support@typhonjs.io>
2024-03-04 21:08:07 -0800

    Added test resources for package.json imports.

---
+++ test/fixture/src/generate/javascript/bundlePackageExports-#imports/index.js
@@ -0,0 +1,7 @@
+// Tests `imports` from `package.json` parsing / loading.
+// "#importsForTesting": "@esm-d-ts-test/org"
+// "#importsForTesting2": "esm-d-ts-test-default"
+
+export * from '#importsForTesting';
+export * from 'esm-d-ts-test-basic';
+export * from '#importsForTesting2';
