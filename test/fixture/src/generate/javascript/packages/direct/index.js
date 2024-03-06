d919195dde3bc990c48418542ec99acd5cbfb8de
typhonrt <support@typhonjs.io>
2024-03-05 22:53:41 -0800

    Update packages test source.

---
+++ test/fixture/src/generate/javascript/packages/direct/index.js
@@ -0,0 +1,3 @@
+export * from '@esm-d-ts-test/org';
+export * from 'esm-d-ts-test-basic';
+export * from 'esm-d-ts-test-default';
--- test/fixture/src/generate/javascript/packages/imports/resolve/index.js
+++ test/fixture/src/generate/javascript/packages/imports/resolve/index.js
@@ -1,7 +1,7 @@
 // Tests `imports` from `package.json` parsing / loading.
-// "#importsForTesting": "@esm-d-ts-test/org"
+// "#importsForTesting/*": "@esm-d-ts-test/*" -> `org` is the sub-path.
 // "#importsForTesting2": "esm-d-ts-test-default"
 
-export * from '#importsForTesting';
+export * from '#importsForTesting/org';
 export * from 'esm-d-ts-test-basic';
 export * from '#importsForTesting2';
