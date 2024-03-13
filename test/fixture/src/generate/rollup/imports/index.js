30f27eaff3257603b42128dc606405dd2509457f
typhonrt <support@typhonjs.io>
2024-03-13 16:36:18 -0700

    Tests for Rollup plugin options for `onwarn` and `outputExt`.

--- test/fixture/src/generate/rollup/imports/index.js
+++
@@ -1,2 +0,0 @@
-export * from '#importsForTesting';
-export * from '#importsForTesting';
--- test/src/generate/rollup-plugin-basic.test.js
+++ test/src/generate/rollup-plugin-basic.test.js
@@ -5,7 +5,8 @@ import fs               from 'fs-extra';
 import { rollup }       from 'rollup';
 import {
    beforeAll,
-   expect }             from 'vitest';
+   expect,
+   vi }                 from 'vitest';
 
 import { generateDTS }  from '../../../src/generator/index.js';
 
@@ -97,6 +98,58 @@ describe('Rollup Plugin (generate)', () =>
          const result = fs.readFileSync('./test/fixture/output/generate/rollup/basic/dtsReplace/index.d.ts', 'utf-8');
 
          expect(result).toMatchFileSnapshot('../../fixture/snapshot/generate/rollup/basic/dtsReplace/index.d.ts');
+      });
+
+      it(`'outputExt' plugin / option`, async () =>
+      {
+         const rollupConfig = {
+            input: {
+               input: './test/fixture/src/generate/javascript/valid/index.js',
+               plugins: [generateDTS.plugin({
+                  compilerOptions: { outDir: './test/fixture/output/generate/rollup/basic/outputExt/.dts' },
+                  outputExt: '.d.mts'
+               })]
+            },
+            output: {
+               file: './test/fixture/output/generate/rollup/basic/outputExt/index.js',
+               format: 'es',
+            }
+         };
+
+         const bundle = await rollup(rollupConfig.input);
+         await bundle.write(rollupConfig.output);
+         await bundle.close();
+
+         const result = fs.readFileSync('./test/fixture/output/generate/rollup/basic/outputExt/index.d.mts', 'utf-8');
+
+         expect(result).toMatchFileSnapshot('../../fixture/snapshot/generate/javascript/valid/index.d.ts');
+      });
+
+      it(`'onwarn' (direct Rollup configuration)`, async () =>
+      {
+         const mock = { onwarn: () => null };
+         const mocked = vi.spyOn(mock, 'onwarn');
+
+         const rollupConfig = {
+            input: {
+               input: './test/fixture/src/generate/javascript/warning/index.js',
+               plugins: [generateDTS.plugin({
+                  compilerOptions: { outDir: './test/fixture/output/generate/rollup/basic/onwarn/.dts' },
+               })],
+               // Set an `onwarn` callback directly.
+               onwarn: () => mock.onwarn()
+            },
+            output: {
+               file: './test/fixture/output/generate/rollup/basic/onwarn/index.js',
+               format: 'es',
+            }
+         };
+
+         const bundle = await rollup(rollupConfig.input);
+         await bundle.write(rollupConfig.output);
+         await bundle.close();
+
+         expect(mocked).toHaveBeenCalled();
       });
    });
 });
